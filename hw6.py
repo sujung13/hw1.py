ffdf85afdd879d9bee22ff0dc6f0d9d383e73d38
sujung13 <163294647+sujung13@users.noreply.github.com>
2024-04-25 21:08:47 +0900

    Add files via upload

---
+++ hw6.py
@@ -0,0 +1,11 @@
+for i in range(1, 10):
+    for k in range(2, 6):
+        print(f'{k} * {i} = {k * i:2d}', end='     ')
+    print()
+
+print()
+
+for i in range(1, 10):
+    for k in range(6, 10):
+        print(f'{k} * {i} = {k * i:2d}', end = '     ')
+    print()
